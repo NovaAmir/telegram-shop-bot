253867b52c6df6ee90de69d69d3c8027262ce58d
Amirmahdi piri <amirmahdipiri110@gmail.com>
2025-11-12 22:58:21 +0330

    Update telegram-bot-shop-new.py

--- telegram-bot-shop-new.py
+++ telegram-bot-shop-new.py
@@ -214,7 +214,7 @@ CATALOG: Dict[str,Dict[str,List[Dict]]] = {
               } , 
             {"id":"women-pants-mazerati-raste-kerem" , 
              "name":"شلوار زنانه مدل ریتا مازراتی راسته رنگ کرم روشن" ,
-             "thumbnail":"https://raw.githubusercontent.com/NovaAmir/telegram_shop_image/main/20251112222400589692652.jpg" , 
+             "thumbnail":"https://github.com/NovaAmir/telegram_shop_image/raw/main/20251112222400589692652.jpg" , 
              "price":560_000 , 
              "sizes":{"44":3 , "46":3 , "50":2 , "52":4}
             }
@@ -454,8 +454,8 @@ async def show_products(update:Update, context:ContextTypes.DEFAULT_TYPE, gender
             else:
                 await q.message.reply_text(caption, reply_markup=keyboard)
         except Exception as e:
-            logger.error(f"Failed to send photo for product {p.get('id', 'Unknown')}: {e}. Falling back to text.")
-            await q.message.reply_text(f"⚠️ خطای نمایش عکس:\n{caption}", reply_markup=keyboard)
+            logger.error(f"Failed to send photo for product {p.get('id', 'Unknown')}: {e}")
+            await q.message.reply_text(f"⚠️ خطای نمایش عکس {p['name']}", reply_markup=keyboard)
 
 
     # پیام راهنما و دکمه بازگشت
@@ -1306,6 +1306,7 @@ if __name__ == "__main__":
     # اگر در محیط رندر هستید، فلش اپ را با هاست 0.0.0.0 و پورت مشخص شده اجرا کنید
     # در غیر این صورت، می‌توانید برای تست لوکال از حالت debug=True استفاده کنید.
     flask_app.run(host="0.0.0.0", port=port, debug=False)
+
 
 
 
