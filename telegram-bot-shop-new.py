68a26a0d43b69b93ad38aa10e5b563a872a356e5
Amirmahdi piri <amirmahdipiri110@gmail.com>
2025-11-12 20:43:24 +0330

    Update telegram-bot-shop-new.py

--- telegram-bot-shop-new.py
+++ telegram-bot-shop-new.py
@@ -214,7 +214,7 @@ CATALOG: Dict[str,Dict[str,List[Dict]]] = {
               } , 
             {"id":"women-pants-mazerati-raste-kerem" , 
              "name":"شلوار زنانه مدل ریتا مازراتی راسته رنگ کرم روشن" ,
-             "thumbnail":"https://raw.githubusercontent.com/NovaAmir/telegram_shop_image/main/f35b4d8a4dd5d4587acbdbd1e79c9ddf8a4b6c41_1742219785.webp" , 
+             "thumbnail":"https://github.com/NovaAmir/telegram_shop_image/raw/main/f35b4d8a4dd5d4587acbdbd1e79c9ddf8a4b6c41_1742219785.webp" , 
              "price":560_000 , 
              "sizes":{"44":3 , "46":3 , "50":2 , "52":4}
             }
@@ -1306,6 +1306,7 @@ if __name__ == "__main__":
     # اگر در محیط رندر هستید، فلش اپ را با هاست 0.0.0.0 و پورت مشخص شده اجرا کنید
     # در غیر این صورت، می‌توانید برای تست لوکال از حالت debug=True استفاده کنید.
     flask_app.run(host="0.0.0.0", port=port, debug=False)
+
 
 
 
